26e07937d446b0616e9456bea624a6e4497c30e8
shauryareddy09-cmyk <shauryareddy09@gmail.com>
2026-06-05 20:10:35 +0530

    Add files via upload

---
+++ New folder/main.py
@@ -0,0 +1,14 @@
+print("Hello")
+print("My name is Shaurya")
+print("my hobby is cycling")
+print(3+9)
+a=12
+b=23
+c=a+b
+print(a+b)
+print("The sum of a and b is",a+b)
+print("The sum of a and b is",c)
+print("The sum of",a,"and",b,"is",c,)
+import keyword
+
+print(keyword.kwlist)
